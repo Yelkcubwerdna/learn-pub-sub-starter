38f5016bf40724bd3c0e5697107c95d486ae6d33
Yelkcubwerdna <yelkcubwerdna@yahoo.com>
2026-05-16 16:01:55 -0400

    05-16-2026

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+
+	connect_str := "amqp://guest:guest@localhost:5672/"
+
+	con, err := amqp.Dial(connect_str)
+	if err != nil {
+		fmt.Printf("Connection failed: %v\n", err)
+		os.Exit(1)
+	}
+	defer con.Close()
+
+	fmt.Println("Connected Successfully")
+
+	rabbit_chan, err := con.Channel()
+	if err != nil {
+		fmt.Print("Failed to open channel: ", err)
+		os.Exit(1)
+	}
+
+	pubsub.PublishJSON(rabbit_chan, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: true})
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+
+	_ = <-signalChan
+	fmt.Println("Program is shutting down...")
 }
