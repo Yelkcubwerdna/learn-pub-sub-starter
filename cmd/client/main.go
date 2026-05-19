fee064fc9f82af1ab0a271404bb321e18da9053f
Yelkcubwerdna <yelkcubwerdna@yahoo.com>
2026-05-19 16:47:49 -0400

    05-19-2026

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril client...")
+
+	connect_str := "amqp://guest:guest@localhost:5672/"
+
+	con, err := amqp.Dial(connect_str)
+	if err != nil {
+		fmt.Printf("Connection failed: %v\n", err)
+		os.Exit(1)
+	}
+	defer con.Close()
+
+	fmt.Println("Connected Successfully")
+
+	username, err := gamelogic.ClientWelcome()
+	if err != nil {
+		fmt.Println("Error with username: ", err)
+	}
+
+	_, _, err = pubsub.DeclareAndBind(con,
+		routing.ExchangePerilDirect,
+		fmt.Sprintf("%s.%s", routing.PauseKey, username),
+		routing.PauseKey,
+		pubsub.Transient,
+	)
+	if err != nil {
+		fmt.Println("Error during declare and bind: ", err)
+		os.Exit(1)
+	}
+
+	// Wait for Ctrl+C signal to exit
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+
+	_ = <-signalChan
+	fmt.Println("Program is shutting down...")
 }
--- internal/pubsub/pubsub.go
+++ internal/pubsub/pubsub.go
@@ -8,6 +8,13 @@ import (
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
+type SimpleQueueType string
+
+const (
+	Durable   SimpleQueueType = "durable"
+	Transient SimpleQueueType = "transient"
+)
+
 func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
 	marshalVal, err := json.Marshal(val)
 	if err != nil {
@@ -20,4 +27,41 @@ func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
 	})
 
 	return nil
+}
+
+func DeclareAndBind(
+	conn *amqp.Connection,
+	exchange,
+	queueName,
+	key string,
+	queueType SimpleQueueType,
+) (*amqp.Channel, amqp.Queue, error) {
+	newChan, err := conn.Channel()
+	if err != nil {
+		return &amqp.Channel{}, amqp.Queue{}, fmt.Errorf("Couldn't open new channel: ", err)
+	}
+
+	newQueue, err := newChan.QueueDeclare(
+		queueName,
+		queueType == Durable,
+		queueType == Transient,
+		queueType == Transient,
+		false,
+		nil,
+	)
+	if err != nil {
+		return &amqp.Channel{}, amqp.Queue{}, fmt.Errorf("Couldn't open new queue: ", err)
+	}
+
+	err = newChan.QueueBind(queueName,
+		key,
+		exchange,
+		false,
+		nil,
+	)
+	if err != nil {
+		return &amqp.Channel{}, amqp.Queue{}, fmt.Errorf("Couldn't bind queue: ", err)
+	}
+
+	return newChan, newQueue, nil
 }
